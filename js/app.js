08649fa404676d1824cc69de4b410bf708ee773e
aabntlal680-ux <aabntlal680@gmail.com>
2026-08-27 22:49:09 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -544,7 +544,7 @@ function wireEmojiPicker() {
 // ---------------------------------------------------------------
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("./service-worker.js").catch(() => {});
+    navigator.serviceWorker.register("/service-worker.js").catch((err) => console.log(err));
   });
 }
 
