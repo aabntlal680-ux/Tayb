ae9c448a4ea483413019574b0e8974057a839e9f
aabntlal680-ux <aabntlal680@gmail.com>
2026-08-31 02:12:12 +0300

    Update push.js

--- js/push.js
+++ js/push.js
@@ -17,12 +17,12 @@ import {
 // ---------------------------------------------------------------
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyDeg6RBNC9bWw1QYxBkYtCuMMFPBzxpw4o",
+  authDomain: "studio-6422025604-b97aa.firebaseapp.com",
+  projectId: "studio-6422025604-b97aa",
+  storageBucket: "studio-6422025604-b97aa.firebasestorage.app",
+  messagingSenderId: "599267399266",
+  appId: "1:599267399266:web:329e49e24298af60f5e33b"
 };
 
 // ---------------------------------------------------------------
