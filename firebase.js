6f82e395d3de0f6d52b367a5c89d963751827945
aabntlal680-ux <aabntlal680@gmail.com>
2026-08-31 01:38:44 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDeg6RBNC9bWw1QYxBkYtCuMMFPBzxpw4o",
+  authDomain: "studio-6422025604-b97aa.firebaseapp.com",
+  projectId: "studio-6422025604-b97aa",
+  storageBucket: "studio-6422025604-b97aa.firebasestorage.app",
+  messagingSenderId: "599267399266",
+  appId: "1:599267399266:web:329e49e24298af60f5e33b"
+};
